67c30b1ab4581ae81c6f234a56f910c671ca4f5f
Ahmetbasli <68350525+Ahmetbasli@users.noreply.github.com>
2021-05-27 09:39:38 +0300

    this is a commit message

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
